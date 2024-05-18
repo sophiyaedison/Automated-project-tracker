315e9657d1cc93f02d4ce176df8f394b569bcae9
sophiyaedison <143161583+sophiyaedison@users.noreply.github.com>
2024-05-18 13:17:37 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 
+// Define user schema
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
 });
 
+// Create User model
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
+
 
